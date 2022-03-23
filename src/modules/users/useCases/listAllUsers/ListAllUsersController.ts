51161fa1b9c1c16fd74d9203aefa5818011389b8
JeffLacerda00 <jefferson.lacerda@garupa.co>
2022-03-23 14:04:04 -0300

    Fix buggs on tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,10 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.body;
+    const allUser = this.listAllUsersUseCase.execute(user_id);
+
+    return response.json(allUser);
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => console.log("🚀 Server started on port 3333!"));
